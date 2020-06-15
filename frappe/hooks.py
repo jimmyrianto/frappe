52e44a353c60727f91df154fe7ca42891a9c7e13
JimmyRianto <jimmy.rianto@gmail.com>
2020-06-15 16:14:02 +0700

    change version

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -12,7 +12,7 @@ source_link = "https://github.com/frappe/frappe"
 app_license = "MIT"
 app_logo_url = '/assets/frappe/images/frappe-framework-logo.png'
 
-develop_version = '12.x.x-develop'
+develop_version = '12.x.x-unisnack'
 
 app_email = "info@frappe.io"
 
